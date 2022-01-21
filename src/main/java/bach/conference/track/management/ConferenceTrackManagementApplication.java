5586acfbb49c21c07b2f2c29b2ea129cb4393331
Hoang Bach Le Le <lele100@uni-duesseldorf.de>
2022-01-21 19:20:35 +0100

    Add Checkstyle, PMD, SpotBugs

--- src/main/java/bach/conference/track/management/ConferenceTrackManagementApplication.java
+++ src/main/java/bach/conference/track/management/ConferenceTrackManagementApplication.java
@@ -3,10 +3,11 @@ package bach.conference.track.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class ConferenceTrackManagementApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(ConferenceTrackManagementApplication.class, args);
     }
 
